2181a16ac877d8dee2be532bcaea94d39f73d61b
mariateresepettersson <mariateresepettersson>
2023-12-05 20:44:15 +0100

    Created login endpoint

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ import bcrypt from "bcryptjs"; // 6
 
 //Setting up the db and creating the cluster auth //2
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth"
-mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
 // 5 Create a user object that has an access token 
 const User = mongoose.model('User', {
   name: {
-    type: String, 
+    type: String,
     unique: true
   },
   email: {
@@ -28,6 +28,17 @@ const User = mongoose.model('User', {
     default: () => crypto.randomBytes(128).toString("hex") // Creates a random unique string of 128 characters
   }
 });
+
+// 8 Create a middleware defining using express = this will tell express to continue execution if everything is OK
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({ accessToken: req.header("Authorization") }); // Header is called authorization
+  if (user) {
+    req.user = user; //attach the user object to the request
+    next();
+  } else {
+    res.status(401).json({ loggedOut: true }); //If false
+  }
+};
 
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
@@ -47,18 +58,33 @@ app.get("/", (req, res) => {
 
 // 7 POST endpoint try to register the user
 app.post("/users", async (req, res) => {
-  try { 
-    const {name, email, password} = req.body; //retreive the name, email and password from the json rquest body
-    const user = new User({name, email, password: bcrypt.hashSync(password)}) // create an instance of the mongoose user model, use hash function to store the hashed value of the password
+  try {
+    const { name, email, password } = req.body; //retreive the name, email and password from the json rquest body
+    const user = new User({ name, email, password: bcrypt.hashSync(password) }) // create an instance of the mongoose user model, use hash function to store the hashed value of the password
     user.save(); //save user to the DB
-    res.status(201).json({id: user._id, accessToken: user.accessToken}); //feedback if successful
-  } catch(err) {
-    res.status(400).json({message: "Could not create user", errors:err.errors});
+    res.status(201).json({ id: user._id, accessToken: user.accessToken }); //feedback if successful
+  } catch (err) {
+    res.status(400).json({ message: "Could not create user", errors: err.errors });
   }
 });
 
-app.get ("/secrets", (req, res) => { // 3
-  res.json({secret: "This is a super secret message."});
+// 9 
+app.get("/secrets", authenticateUser); // 9 Protect endpoint 
+app.get("/secrets", (req, res) => { // 3
+  res.json({ secret: "This is a super secret message." });
+});
+
+// 10 - allow user to login
+// req.body.password = declare password from the request
+// user.password = hashed password that is tsored in the DB
+// bcrypt.compareSync = chechs if the 2 lines above are equal
+app.post("/sessions", async (req, res) => {
+  const user = await User.findOne({email: req.body.email}) //getting user from db, expecting user to give email and password to login
+  if(user && bcrypt.compareSync(req.body.password, user.password)) { //if the user returned from the DB exists and that the password sent in the json body matches the password that we have stored in the database
+    res.json({userId: user._id, accessToken: user.accessToken});
+  } else {
+    res.json({notFound: true});
+  }
 });
 
 // Start the server
